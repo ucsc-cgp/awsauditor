61242797dbb13b0ce2e8bd94cd79aab9471e77aa
emlys <esoth@ucsc.edu>
2019-02-04 15:06:49 -0800

    clarifying comment

--- package/app.py
+++ package/app.py
@@ -7,7 +7,8 @@ app = Chalice(app_name='package')
 def lambda_handler(event, context):
     awsAuditor.main()
 
-
+# Option to set up the lambda to run on a schedule specified in cron format
+# This example will run at 8:30 pm UTC every day
 # @app.schedule('cron(30, 20, *, *, ?, *)')
 # def lambda_handler(event):
 #     awsAuditor.main()
