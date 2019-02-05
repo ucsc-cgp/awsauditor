037073f7fbf87a35acba10010753621306a88a33
bencvdb <bencvdb93@gmail.com>
2019-02-04 19:52:47 -0800

    Grab recipient info from s3 bucket

--- awsAuditor.py
+++ awsAuditor.py
@@ -1,4 +1,6 @@
 import datetime
+import boto3
+import json
 from reportGenerator import ReportGenerator
 
 """
@@ -6,20 +8,41 @@ Send month-to-date account management reports and individualized reports to spec
 """
 
 
+def get_recipients(bucket, path):
+    """
+    Determine the recipients for the management-focused and individual reports.
+
+    The file stored at `path` in `bucket` must have a valid json structure. The dictionary containing info for managers
+    must be associated with the 'managers'. The list containing the users to receive reports needs to be associated with
+    'users'.
+
+    :param str bucket: the name of the bucket where the recipient info is stored.
+    :param str path: The path to the file in 'bucket'.
+    :return dict, list: The dictionary that associates managers and the accounts they want reports for and the list of
+                        users to receive individual reports.
+    """
+    s3 = boto3.client('s3')
+
+    file = s3.get_object(Bucket=bucket, Key=path)
+    j = json.loads(file['Body'].read())
+
+    return j['managers'], j['users']
+
+
 def main():
     start = str(datetime.date.today().replace(day=1))
     end = str(datetime.date.today())
 
-    manager_accounts = {'manager1@email.com': ['account1', 'account2'],
-                        'manager2@email.com': ['account2', 'account3']}
+    bucket_name = 'bucket-with-file'
+    file_name = 'file_in_bucket.json'
 
-    users = ['user1@email.com', 'user2@email.com']
+    manager_accounts, users = get_recipients(bucket_name, file_name)
 
     r = ReportGenerator(start_date=start, end_date=end)
 
     # Send account management reports
     for manager, accounts in manager_accounts.items():
-        r.send_management_report(manager, accounts)
+        r.send_management_report([manager], accounts)
 
     # Send individual reports
     for user in users:
